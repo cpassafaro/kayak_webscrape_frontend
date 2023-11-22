2ab00dd8bcc5217b30f4248a3132f50c3c13a73a
Christina Passafaro <christina.passafaro@gmail.com>
2023-11-22 14:54:39 -0500

    need to set up vuetify global stylesheet

--- src/components/KayakCard.vue
+++ src/components/KayakCard.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-card class="ma-2 container-card">
+  <v-card class="ma-2 pa-3 elevation-0 container-card" variant="outlined">
+    <v-card-title>{{ value.title }}</v-card-title>
+    <v-card-subtitle>{{ value.website }}</v-card-subtitle>
     <v-row>
       <v-col>
-        <div>Picture Below</div>
         <v-img :src="imgSource" ></v-img>
       </v-col>
     </v-row>
-    {{ value.title }}
+    <v-card-text>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn prepend-icon="mdi-link" variant="tonal" class="pa-2 btn-href"><a :href="value.link" target="_blank">Click to go to site</a></v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -24,15 +29,12 @@ export default {
   },
   mounted() {
     console.log('kayak card', this.value)
-    console.log(this.value.title.toLowerCase().includes('ripper'))
+    // console.log(this.value.title.toLowerCase().includes('ripper'))
     if(this.value.title.toLowerCase().includes('ripper')) {
-      console.log('ripper')
       this.imgSource = 'https://www.pyranha.com/images/products/ripper/ripper_rr_ts@2x.jpg'
     } else if (this.value.title.toLowerCase().includes('remix') || this.value.title.toLowerCase().includes('rmx')) {
-      console.log('remix')
       this.imgSource = 'https://liquidlogickayaks.com/cdn/shop/products/Remix_69_Bluegrass.jpg?v=1639577342'
     } else if (this.value.title.toLowerCase().includes('mixmaster')) {
-      console.log('mixmaster')
       this.imgSource = 'https://www.backcountry.com/images/items/large/JAK/JAK0053/RD.jpg'
     }
   }
@@ -42,5 +44,10 @@ export default {
 <style scoped>
 .container-card {
   /* height: 30px; */
+  min-height: 400px;
+}
+.btn-href {
+  /* background-color: black; */
+  color: white;
 }
 </style>
